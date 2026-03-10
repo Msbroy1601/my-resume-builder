aa5ff5a80c31b37a5feef03cc2ea625d9f552b75
Msbroy1601 <baishaliroy11@gmail.com>
2026-03-10 23:48:53 +0530

    Fix redirect on ClerkProvider

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignInUrl="/templates" afterSignUpUrl="/templates">
       <BrowserRouter>
         <App />
       </BrowserRouter>
